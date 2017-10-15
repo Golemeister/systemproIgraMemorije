819385a1fffc5727779d6624b1a23b4e5e068a91
Golemeister <fire8venox@gmail.com>
2017-10-15 11:40:13 +0200

    nadogradnja

--- app/src/main/java/rs/systempro/igramemorije/GameActivity.java
+++ app/src/main/java/rs/systempro/igramemorije/GameActivity.java
@@ -1,19 +1,32 @@
 package rs.systempro.igramemorije;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import static rs.systempro.igramemorije.R.id.glMreza;
 
 public class GameActivity extends AppCompatActivity {
 
     int h;
     int v;
+    ArrayList<Drawable> images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +36,63 @@ public class GameActivity extends AppCompatActivity {
         Intent myIntent = getIntent();
         h= myIntent.getIntExtra("height", 3);
         v = myIntent.getIntExtra("width",3);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+        images = new ArrayList<>();
+       for(int i=0;i<(h*v)/2;i++)
+       {
 
-        GridLayout glMreza = (GridLayout) findViewById(R.id.glMreza);
+           int resid =  getResources().getIdentifier("slicica"+(i+1),"drawable",getPackageName());
 
-        if(glMreza.getChildCount()!=0) {
-        return;
-        }
+           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+               images.add(getDrawable(resid));
+               images.add(getDrawable(resid));
+           }
+           else {
+               images.add(getResources().getDrawable(resid));
+               images.add(getResources().getDrawable(resid));
 
+
+           }
+           Collections.shuffle(images);
+       }
+
+        final RelativeLayout rlRoot = (RelativeLayout)findViewById(R.id.rlRoot);
+        final GridLayout glMreza = (GridLayout) findViewById(R.id.glMreza);
         glMreza.setColumnCount(v);
+        rlRoot.post(new Runnable() {
+            @Override
+            public void run() {
+                int rootH = rlRoot.getHeight();
+                int rootW = rlRoot.getWidth();
 
-        for (int i=0;i<h*v;i++)
-        {
-            Button b = new Button(this);
-            b.setText("Dugme"+i);
-            glMreza.addView(b);
-            GridLayout.LayoutParams param= new GridLayout.LayoutParams();
-            param.height= GridLayout.LayoutParams.WRAP_CONTENT;
-            param.setGravity(Gravity.FILL);
-            b.setLayoutParams(param);
-        }
+                Toast.makeText(getApplicationContext(), rootH + " - " + rootW, Toast.LENGTH_SHORT).show();
+                for (int i = 0; i < h * v; i++) {
+                    ImageButton b = new ImageButton(getApplicationContext());
+
+
+
+                    b.setBackgroundColor(Color.RED);
+                    b.setImageDrawable(images.get(i));
+                    glMreza.addView(b);
+                    GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+                    if(rootH/h <rootW/v) {
+                        param.height = rootH / h - 20;
+                        param.width = rootH / h -20;
+                    }
+                    else {
+                        param.width = rootW / v - 20;
+                        param.height = rootW / v- 20;
+                    }
+                    param.setMargins(10,10,10,10);
+                    b.setLayoutParams(param);
+                }
+
+
+                 }
+        });
 
 
     }
+
+
 }
