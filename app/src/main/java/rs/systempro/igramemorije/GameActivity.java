7f60373dc46bc448fd4f90061d76335bca4d88a8
Golemeister <fire8venox@gmail.com>
2017-10-21 11:39:39 +0200

    nadogradnja 2

--- app/src/main/java/rs/systempro/igramemorije/GameActivity.java
+++ app/src/main/java/rs/systempro/igramemorije/GameActivity.java
@@ -13,20 +13,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static rs.systempro.igramemorije.R.id.glMreza;
 
 public class GameActivity extends AppCompatActivity {
 
-    int h;
-    int v;
+    int solve;
     ArrayList<Drawable> images;
+    ImageButton firstOpened;
+    ImageButton secondOpened;
+    int numOfOpened;
+    int time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +40,12 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         Intent myIntent = getIntent();
-        h= myIntent.getIntExtra("height", 3);
-        v = myIntent.getIntExtra("width",3);
+        final int h= myIntent.getIntExtra("height", 3);
+        final int v = myIntent.getIntExtra("width",3);
 
         images = new ArrayList<>();
+        solve = 0;
+        numOfOpened = 0;
        for(int i=0;i<(h*v)/2;i++)
        {
 
@@ -58,7 +66,19 @@ public class GameActivity extends AppCompatActivity {
 
         final RelativeLayout rlRoot = (RelativeLayout)findViewById(R.id.rlRoot);
         final GridLayout glMreza = (GridLayout) findViewById(R.id.glMreza);
+
+
         glMreza.setColumnCount(v);
+
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                time++;
+            }
+        },1000,1000);
+
+
         rlRoot.post(new Runnable() {
             @Override
             public void run() {
@@ -67,12 +87,15 @@ public class GameActivity extends AppCompatActivity {
 
                 Toast.makeText(getApplicationContext(), rootH + " - " + rootW, Toast.LENGTH_SHORT).show();
                 for (int i = 0; i < h * v; i++) {
-                    ImageButton b = new ImageButton(getApplicationContext());
-
+                    final ImageButton b = new ImageButton(getApplicationContext());
+                    final Drawable image = images.get(i);
 
 
                     b.setBackgroundColor(Color.RED);
-                    b.setImageDrawable(images.get(i));
+                    //b.setImageDrawable(images.get(i));
+                    b.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    b.setPadding(0,0,0,0);
+
                     glMreza.addView(b);
                     GridLayout.LayoutParams param = new GridLayout.LayoutParams();
                     if(rootH/h <rootW/v) {
@@ -85,6 +108,44 @@ public class GameActivity extends AppCompatActivity {
                     }
                     param.setMargins(10,10,10,10);
                     b.setLayoutParams(param);
+
+                    b.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //b.setImageDrawable(image);
+                            if(numOfOpened == 2)
+                            {
+                                numOfOpened=numOfOpened-1;
+                                //b.setImageDrawable(image);
+                                firstOpened.setImageDrawable(null);
+                                secondOpened.setImageDrawable(null);
+                                b.setImageDrawable(image);
+                                firstOpened = b;
+                            }
+                            else if(numOfOpened == 1) {
+
+                                b.setImageDrawable(image);
+                                numOfOpened++;
+                                secondOpened = b;
+                                b.setImageDrawable(image);
+                                if(firstOpened.getDrawable().equals(secondOpened.getDrawable()))
+                                {
+                                    numOfOpened = 0;
+                                    firstOpened.setClickable(false);
+                                    secondOpened.setClickable(false);
+
+                                }
+                            }
+                            else
+                            {
+                                numOfOpened = 1;
+                                firstOpened = b;
+                            }
+
+
+                        }
+                    });
+
                 }
 
 
